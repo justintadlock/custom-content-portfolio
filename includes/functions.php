9d8188861c545d66e0f118bf45e72f03352a3521
Justin Tadlock <justin@justintadlock.com>
2013-01-25 02:03:02 -0600

    Use '%portfolio%' in the default portfolio item permalink.  Also, I added in support for Breadcrumb Trail, which, with a little hacking, will support this permalink structure.

--- includes/functions.php
+++ includes/functions.php
@@ -17,6 +17,9 @@ add_filter( 'post_type_archive_title', 'cc_portfolio_post_type_archive_title' );
 /* Filter the post type permalink. */
 add_filter( 'post_type_link', 'cc_portfolio_post_type_link', 10, 2 );
 
+/* Filter the breadcrumb trail items (Breadcrumb Trail script/plugin). */
+add_filter( 'breadcrumb_trail_items', 'cc_portfolio_breadcrumb_trail_items' );
+
 /**
  * Returns the default settings for the plugin.
  *
@@ -29,7 +32,7 @@ function cc_portfolio_get_default_settings() {
 	$settings = array(
 		'portfolio_root'      => 'portfolio',
 		'portfolio_base'      => '',          // defaults to 'portfolio_root'
-		'portfolio_item_base' => 'item'
+		'portfolio_item_base' => '%portfolio%'
 	);
 
 	return $settings;
@@ -70,7 +73,7 @@ function cc_portfolio_post_type_link( $post_link, $post ) {
 		return $post_link;
 
 	/* Allow %portfolio% in the custom post type permalink. */
-	if ( strpos( $post_link, '%portfolio%' ) ) {
+	if ( false !== strpos( $post_link, '%portfolio%' ) ) {
 	
 		/* Get the terms. */
 		$terms = get_the_terms( $post, 'portfolio' ); // @todo apply filters to tax name.
@@ -88,6 +91,60 @@ function cc_portfolio_post_type_link( $post_link, $post ) {
 	}
 
 	return $post_link;
+}
+
+/**
+ * Filters the 'breadcrumb_trail_items' hook from the Breadcrumb Trail plugin and the script version 
+ * included in the Hybrid Core framework.  At best, this is a neat hack to add the portfolio to the 
+ * single view of portfolio items based off the '%portfolio%' rewrite tag.  At worst, it's potentially 
+ * a huge management nightmare in the long term.  A better solution is definitely needed baked right 
+ * into Breadcrumb Trail itself that takes advantage of its built-in features for figuring out this type 
+ * of thing.
+ *
+ * @since  0.1.0
+ * @access public
+ * @param  array  $items
+ * @return array
+ */
+function cc_portfolio_breadcrumb_trail_items( $items ) {
+
+	if ( is_singular( 'portfolio_item' ) ) {
+
+		$settings = get_option( 'plugin_cc_portfolio', cc_portfolio_get_default_settings() );
+
+		if ( false !== strpos( $settings['portfolio_item_base'], '%portfolio%' ) ) {
+			$post_id = get_queried_object_id();
+
+			$terms = get_the_terms( $post_id, 'portfolio' );
+
+			if ( !empty( $terms ) ) {
+
+				usort( $terms, '_usort_terms_by_ID' );
+				$term = get_term( $terms[0], 'portfolio' );
+				$term_id = $term->term_id;
+
+				$parents = array();
+
+				while ( $term_id ) {
+
+					/* Get the parent term. */
+					$term = get_term( $term_id, 'portfolio' );
+
+					/* Add the formatted term link to the array of parent terms. */
+					$parents[] = '<a href="' . get_term_link( $term, 'portfolio' ) . '" title="' . esc_attr( $term->name ) . '">' . $term->name . '</a>';
+
+					/* Set the parent term's parent as the parent ID. */
+					$term_id = $term->parent;
+				}
+
+				$items   = array_splice( $items, 0, -1 );
+				$items   = array_merge( $items, array_reverse( $parents ) );
+				$items[] = single_post_title( '', false );
+			}
+		}
+	}
+
+	return $items;
 }
 
 ?>
