ce537e2f8cefb80fca0cec0149b6f0493071e4de
Justin Tadlock <justin@justintadlock.com>
2015-10-21 09:31:01 -0500

    Update category taxonomy labels. Adds the `not_found` label for all taxonomies.

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -22,9 +22,6 @@ add_action( 'init', 'ccp_register_taxonomies', 9 );
  */
 function ccp_register_taxonomies() {
 
-	// Get the plugin settings.
-	$settings = get_option( 'plugin_custom_content_portfolio', ccp_get_default_settings() );
-
 	// Set up the arguments for the portfolio category taxonomy.
 	$cat_args = array(
 		'public'            => true,
@@ -53,21 +50,21 @@ function ccp_register_taxonomies() {
 
 		// Labels used when displaying taxonomy and terms.
 		'labels' => array(
-			'name'                       => __( 'Portfolios',                           'custom-content-portfolio' ),
-			'singular_name'              => __( 'Portfolio',                            'custom-content-portfolio' ),
-			'menu_name'                  => __( 'Portfolios',                           'custom-content-portfolio' ),
-			'name_admin_bar'             => __( 'Portfolio',                            'custom-content-portfolio' ),
-			'search_items'               => __( 'Search Portfolios',                    'custom-content-portfolio' ),
-			'popular_items'              => __( 'Popular Portfolios',                   'custom-content-portfolio' ),
-			'all_items'                  => __( 'All Portfolios',                       'custom-content-portfolio' ),
-			'edit_item'                  => __( 'Edit Portfolio',                       'custom-content-portfolio' ),
-			'view_item'                  => __( 'View Portfolio',                       'custom-content-portfolio' ),
-			'update_item'                => __( 'Update Portfolio',                     'custom-content-portfolio' ),
-			'add_new_item'               => __( 'Add New Portfolio',                    'custom-content-portfolio' ),
-			'new_item_name'              => __( 'New Portfolio Name',                   'custom-content-portfolio' ),
-			'separate_items_with_commas' => __( 'Separate portfolios with commas',      'custom-content-portfolio' ),
-			'add_or_remove_items'        => __( 'Add or remove portfolios',             'custom-content-portfolio' ),
-			'choose_from_most_used'      => __( 'Choose from the most used portfolios', 'custom-content-portfolio' ),
+			'name'                       => __( 'Categories',                           'custom-content-portfolio' ),
+			'singular_name'              => __( 'Category',                             'custom-content-portfolio' ),
+			'menu_name'                  => __( 'Categories',                           'custom-content-portfolio' ),
+			'name_admin_bar'             => __( 'Category',                             'custom-content-portfolio' ),
+			'search_items'               => __( 'Search Categories',                    'custom-content-portfolio' ),
+			'popular_items'              => __( 'Popular Categories',                   'custom-content-portfolio' ),
+			'all_items'                  => __( 'All Categories',                       'custom-content-portfolio' ),
+			'edit_item'                  => __( 'Edit Category',                        'custom-content-portfolio' ),
+			'view_item'                  => __( 'View Category',                        'custom-content-portfolio' ),
+			'update_item'                => __( 'Update Category',                      'custom-content-portfolio' ),
+			'add_new_item'               => __( 'Add New Category',                     'custom-content-portfolio' ),
+			'new_item_name'              => __( 'New Category Name',                    'custom-content-portfolio' ),
+			'not_found'                  => __( 'No categories found.',                 'custom-content-portfolio' ),
+			'parent_item'                => __( 'Parent Category',                      'custom-content-portfolio' ),
+			'parent_item_colon'          => __( 'Parent Category:',                     'custom-content-portfolio' ),
 		)
 	);
 
@@ -114,6 +111,7 @@ function ccp_register_taxonomies() {
 			'separate_items_with_commas' => __( 'Separate tags with commas',      'custom-content-portfolio' ),
 			'add_or_remove_items'        => __( 'Add or remove tags',             'custom-content-portfolio' ),
 			'choose_from_most_used'      => __( 'Choose from the most used tags', 'custom-content-portfolio' ),
+			'not_found'                  => __( 'No tags found.',                 'custom-content-portfolio' ),
 		)
 	);
 
