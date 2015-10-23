1a806ee090b395368e76d4b1dbaca275b7179a8c
Justin Tadlock <justin@justintadlock.com>
2015-10-22 19:50:18 -0500

    Add template tags for getting and outputting location, start date, and end date metadata.

--- inc/template.php
+++ inc/template.php
@@ -11,7 +11,7 @@
  */
 
 /**
- * Makes sure the post ID is an absolute integer if passed in.  Else, returns the result 
+ * Makes sure the post ID is an absolute integer if passed in.  Else, returns the result
  * of `get_the_ID()`.
  *
  * @since  1.0.0
@@ -179,4 +179,154 @@ function ccp_get_project_client( $args = array() ) {
 	}
 
 	return apply_filters( 'ccp_get_project_client', $html, $args['post_id'] );
+}
+
+/**
+ * Prints the project location.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return void
+ */
+function ccp_project_location( $args = array() ) {
+	echo ccp_get_project_location( $args );
+}
+
+/**
+ * Returns the project location.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return string
+ */
+function ccp_get_project_location( $args = array() ) {
+
+	$html = '';
+
+	$defaults = array(
+		'post_id' => ccp_get_project_id(),
+		'text'    => '%s',
+		'before'  => '',
+		'after'   => '',
+		'wrap'    => '<span %s>%s</span>',
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	$location = ccp_get_project_meta( $args['post_id'], 'location' );
+
+	if ( $location ) {
+
+		$text = sprintf( $args['text'], $location );
+
+		$html .= $args['before'];
+		$html .= sprintf( $args['wrap'], 'class="project-location"', $text );
+		$html .= $args['after'];
+	}
+
+	return apply_filters( 'ccp_get_project_location', $html, $args['post_id'] );
+}
+
+/**
+ * Prints the project start_date.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return void
+ */
+function ccp_project_start_date( $args = array() ) {
+	echo ccp_get_project_start_date( $args );
+}
+
+/**
+ * Returns the project start_date.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return string
+ */
+function ccp_get_project_start_date( $args = array() ) {
+
+	$html = '';
+
+	$defaults = array(
+		'post_id' => ccp_get_project_id(),
+		'text'    => '%s',
+		'format'  => get_option( 'date_format' ),
+		'before'  => '',
+		'after'   => '',
+		'wrap'    => '<time %s>%s</time>',
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	$start_date = ccp_get_project_meta( $args['post_id'], 'start_date' );
+
+	if ( $start_date ) {
+
+		$text = sprintf( $args['text'], mysql2date( $args['format'], $start_date, true ) );
+
+		$datetime = sprintf( 'datetime="%s"', mysql2date( 'Y-m-d\TH:i:sP', $start_date, true ) );
+
+		$html .= $args['before'];
+		$html .= sprintf( $args['wrap'], 'class="project-start-date" ' . $datetime, $text );
+		$html .= $args['after'];
+	}
+
+	return apply_filters( 'ccp_get_project_start_date', $html, $args['post_id'] );
+}
+
+/**
+ * Prints the project end_date.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return void
+ */
+function ccp_project_end_date( $args = array() ) {
+	echo ccp_get_project_end_date( $args );
+}
+
+/**
+ * Returns the project end_date.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return string
+ */
+function ccp_get_project_end_date( $args = array() ) {
+
+	$html = '';
+
+	$defaults = array(
+		'post_id' => ccp_get_project_id(),
+		'text'    => '%s',
+		'format'  => get_option( 'date_format' ),
+		'before'  => '',
+		'after'   => '',
+		'wrap'    => '<time %s>%s</time>',
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	$end_date = ccp_get_project_meta( $args['post_id'], 'end_date' );
+
+	if ( $end_date ) {
+
+		$text = sprintf( $args['text'], mysql2date( $args['format'], $end_date, true ) );
+
+		$datetime = sprintf( 'datetime="%s"', mysql2date( 'Y-m-d\TH:i:sP', $end_date, true ) );
+
+		$html .= $args['before'];
+		$html .= sprintf( $args['wrap'], 'class="project-end-date" ' . $datetime, $text );
+		$html .= $args['after'];
+	}
+
+	return apply_filters( 'ccp_get_project_end_date', $html, $args['post_id'] );
 }
