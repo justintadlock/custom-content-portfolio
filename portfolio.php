82d8acd99adbbd828f20c1c8a84d4bb7d92e5da8
Justin Tadlock <justin@justintadlock.com>
2015-10-31 15:06:27 -0500

    Rename manage projects file to `class-manage-projects.php`.

--- portfolio.php
+++ portfolio.php
@@ -178,10 +178,10 @@ final class CCP_Plugin {
 
 		// Load admin files.
 		if ( is_admin() ) {
-			require_once( $this->dir_path . 'admin/functions-admin.php'    );
-			require_once( $this->dir_path . 'admin/class-projects.php'     );
-			require_once( $this->dir_path . 'admin/class-project-edit.php' );
-			require_once( $this->dir_path . 'admin/class-settings.php'     );
+			require_once( $this->dir_path . 'admin/functions-admin.php'       );
+			require_once( $this->dir_path . 'admin/class-manage-projects.php' );
+			require_once( $this->dir_path . 'admin/class-project-edit.php'    );
+			require_once( $this->dir_path . 'admin/class-settings.php'        );
 		}
 	}
 
