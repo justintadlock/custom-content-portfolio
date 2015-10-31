cb1c862dbeeac75d4bc6bab9372c68757b286158
Justin Tadlock <justin@justintadlock.com>
2015-10-31 15:40:15 -0500

    Bring `uninstall.php` up to date so that we're deleting the correct options and removing the correct capabilities.

--- uninstall.php
+++ uninstall.php
@@ -15,7 +15,12 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 
 /* === Delete plugin options. === */
 
+// Remove pre-1.0.0 options.
 delete_option( 'plugin_custom_content_portfolio' );
+
+// Remove 1.0.0+ options.
+delete_option( 'ccp_settings'        );
+delete_option( 'ccp_sticky_projects' );
 
 /* === Remove capabilities added by the plugin. === */
 
@@ -23,11 +28,27 @@ delete_option( 'plugin_custom_content_portfolio' );
 $role = get_role( 'administrator' );
 
 // If the administrator role exists, remove added capabilities for the plugin.
-if ( ! empty( $role ) ) {
+if ( ! is_null( $role ) ) {
 
-	$role->remove_cap( 'manage_portfolio'          );
-	$role->remove_cap( 'create_portfolio_items'    );
-	$role->remove_cap( 'edit_portfolio_items'      );
-	$role->remove_cap( 'create_portfolio_projects' );
-	$role->remove_cap( 'edit_portfolio_projects'   );
+	// Remove pre-1.0.0 caps.
+	$role->remove_cap( 'manage_portfolio'       );
+	$role->remove_cap( 'create_portfolio_items' );
+	$role->remove_cap( 'edit_portfolio_items'   );
+
+	// Taxonomy caps.
+	$role->remove_cap( 'manage_portfolio_categories' );
+	$role->remove_cap( 'manage_portfolio_tags'       );
+
+	// Post type caps.
+	$role->remove_cap( 'create_portfolio_projects'           );
+	$role->remove_cap( 'edit_portfolio_projects'             );
+	$role->remove_cap( 'edit_others_portfolio_projects'      );
+	$role->remove_cap( 'publish_portfolio_projects'          );
+	$role->remove_cap( 'read_private_portfolio_projects'     );
+	$role->remove_cap( 'delete_portfolio_projects'           );
+	$role->remove_cap( 'delete_private_portfolio_projects'   );
+	$role->remove_cap( 'delete_published_portfolio_projects' );
+	$role->remove_cap( 'delete_others_portfolio_projects'    );
+	$role->remove_cap( 'edit_private_portfolio_projects'     );
+	$role->remove_cap( 'edit_published_portfolio_projects'   );
 }
